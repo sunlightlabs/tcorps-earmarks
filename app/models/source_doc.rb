b364e7da5220330563f0c7ed8d6848382c023538
David James <djames@sunlightfoundation.com>
2009-06-25 18:06:11 -0400

    Fixed up 'grab random SourceDoc' logic.

--- app/models/source_doc.rb
+++ app/models/source_doc.rb
@@ -4,4 +4,11 @@ class SourceDoc < ActiveRecord::Base
   has_many :letters
   belongs_to :legislator
   
+  def self.get_random
+    min = self.minimum(:letters_count)
+    letters = self.find(:all, :conditions =>
+      ["letters_count = ? and conversion_failed = ?", min, false])
+    letters[rand(letters.length)]
+  end
+  
 end
