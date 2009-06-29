53a1ebb44124953c426b29b225f7673099f09c71
klondike <kprojection@gmail.com>
2009-06-28 20:59:53 -0400

    Swallowing connection refused errors

--- lib/tcorps_util.rb
+++ lib/tcorps_util.rb
@@ -5,12 +5,8 @@ class TcorpsUtil
     Rails.logger.info "== Signaling task completion by POSTing to #{url}..."
     response = Net::HTTP.post_form(url, :task_key => task_key)
     Rails.logger.info "   Response : #{response.to_yaml}"
+  rescue Errno::ECONNREFUSED
+    false
   end
-
-  # In the future, we may add this to the method above:
-  # rescue Errno::ECONNREFUSED
-  #   false
-  #
-  # For now, I want to know about any errors that might occur.
-
+  
 end
