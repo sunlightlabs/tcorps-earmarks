05cb632c7963894593154f2e4d83f482326528ee
klondike <kprojection@gmail.com>
2009-07-09 13:32:29 -0400

    Updated dropdown to order by last name and drop the 'Rep.'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,13 @@ module ApplicationHelper
   end
   
   def legislators_for_select
-    Legislator.alphabetical.all.map {|legislator| ["Rep. #{legislator.name}", legislator.id]}
+    Legislator.alphabetical.all.map {|legislator| [reordered_name(legislator.name), legislator.id]}.sort {|x, y| x[0] <=> y[0]}
+  end
+  
+  def reordered_name(name)
+    names = name.split
+    last = names.pop
+    "#{last}, #{names.join ' '}"
   end
 
 end
