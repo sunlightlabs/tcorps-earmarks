8d85d195753aecc6b930757ac1d494d02a280253
klondike <kprojection@gmail.com>
2009-08-06 17:56:24 -0400

    Removed User class

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,10 +2,8 @@ class DocumentsController < ApplicationController
 
   def index
     @source_doc = SourceDoc.get_random
-    @user = User.find_or_create_by_name(params["username"])
     @letter = Letter.new(
       :source_doc_id => @source_doc.id,
-      :user_id       => @user.id,
       :task_key      => params["task_key"]
     )
     @letter.entities << Entity.new
@@ -14,7 +12,7 @@ class DocumentsController < ApplicationController
   end
 
   def create
-    @letter = Letter.new(clean_letter_params(params[:letter]))
+    @letter = Letter.new params[:letter]
     if params["add_new_entity"]
       @letter.entities << Entity.new
       setup_and_render_index
@@ -26,10 +24,10 @@ class DocumentsController < ApplicationController
       setup_and_render_index
     elsif @letter.save
       flash[:notice] = "Thanks for contributing!  We've logged your points in TransparencyCorps. Here's another document so that you can earn more!"
-      if url = TransparencyCorps.complete_task_and_reassign(@letter.task_key)
+      if !@letter.task_key.blank? and url = TransparencyCorps.complete_task_and_reassign(@letter.task_key)
         redirect_to url
       else
-        redirect_to :action => :success
+        redirect_to success_path
       end
     else
       setup_and_render_index
@@ -52,25 +50,6 @@ class DocumentsController < ApplicationController
     @username = params["username"]
     @points   = params["points"] || 0
     @has_plain_text = @source_doc.plain_text_length > 10
-  end
-  
-  protected
-  
-  def clean_letter_params(params)
-    clean_params(params, [
-      :amount,
-      :project_title,
-      :fiscal_year,
-      :funding_purpose,
-      :taxpayer_justification,
-      :user_id,
-      :source_doc_id,
-      :task_key,
-      :legislator_id,
-      # And finally, because of:
-      #   accepts_nested_attributes_for :entities
-      :entities_attributes
-    ])
   end
   
 end
