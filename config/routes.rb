84f2a5520908ba0dda368c549462192eb5d39287
klondike <kprojection@gmail.com>
2009-08-06 17:57:48 -0400

    Added success route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :documents
+  map.success '/success', :controller => 'documents', :action => 'success'
   map.root :controller => 'documents', :action => 'index'
 end
