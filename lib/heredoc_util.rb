f70bee4b45524b3707d1ba26121bab5f6b93ba69
klondike <kprojection@gmail.com>
2009-08-06 17:14:57 -0400

    Removing more unneeded files

--- lib/heredoc_util.rb
+++
@@ -1,7 +0,0 @@
-class HeredocUtil
-
-  def self.prettify(string)
-    string.lines.map { |x| x.lstrip.chomp }.join("\n")
-  end
-  
-end
--- lib/scribd_util.rb
+++
@@ -1,7 +0,0 @@
-class ScribdUtil
-  
-  def self.url(doc_id)
-    "http://www.scribd.com/doc/#{doc_id}"
-  end
-
-end
