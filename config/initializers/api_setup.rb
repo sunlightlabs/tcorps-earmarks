2772e8d5b9ae6485f496806e13d88d85963a1c9e
klondike <kprojection@gmail.com>
2009-08-06 16:35:07 -0400

    Renamed api_setup to scribd

--- config/initializers/api_setup.rb
+++ config/initializers/api_setup.rb
@@ -1,5 +1,5 @@
-API_SETUP = YAML.load_file("#{RAILS_ROOT}/config/api_setup.yml")
+SCRIBD_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/scribd.yml")
 
-Scribd::API.instance.key    = API_SETUP['scribd']['api_key']
-Scribd::API.instance.secret = API_SETUP['scribd']['api_secret']
+Scribd::API.instance.key    = SCRIBD_CONFIG['scribd']['api_key']
+Scribd::API.instance.secret = SCRIBD_CONFIG['scribd']['api_secret']
 Scribd::API.instance.debug  = false
