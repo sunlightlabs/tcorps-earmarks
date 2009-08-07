e2aa1b19642848bfa990709be828059335e78e11
klondike <kprojection@gmail.com>
2009-08-07 18:11:34 -0400

    Further slimmed controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -8,15 +8,19 @@ class DocumentsController < ApplicationController
 
   def create
     @letter = Letter.new params[:letter]
+    @document = @letter.document
+    
     if params[:add_new_entity]
       @letter.entities << Entity.new
-      setup_and_render_index
+      render :index
+    
     elsif params[:delete_entity]
       params[:delete_entity].keys.each do |key|
         entity_id = key.to_i
         @letter.entities.delete_at(entity_id)
       end
-      setup_and_render_index
+      render :index
+      
     elsif @letter.save
       if !@letter.task_key.blank? and url = TransparencyCorps.complete_task_and_reassign(@letter.task_key)
         flash[:notice] = "Thanks for contributing!  We've logged your points in TransparencyCorps. Here's another document so that you can earn more!"
@@ -26,15 +30,8 @@ class DocumentsController < ApplicationController
         redirect_to documents_path
       end
     else
-      setup_and_render_index
+      render :index
     end
-  end
-  
-  protected
-  
-  def setup_and_render_index
-    @document = @letter.document
-    render :action => :index
   end
   
 end
