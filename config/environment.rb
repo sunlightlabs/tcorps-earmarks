e004a0858f37989923b10c24eca1c2bd1762e760
klondike <kprojection@gmail.com>
2009-08-14 12:49:20 -0400

    Bug in how text was being required

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'rscribd'
   config.gem 'sunlightlabs-tcorps', :lib => 'tcorps', :source => 'http://gems.github.com'
-  config.gem 'Text'
+  config.gem 'Text', :lib => 'text'
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
